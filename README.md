90e501616aeef6e98ceb7625511aa0607bc8ee80
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2022-11-08 01:46:50 +0800

    Display question
    todo:
    - pass difficulty into coderoom/index.tsx, then pass into /api/questions/[difficulty]...

--- README.md
+++ README.md
@@ -5,11 +5,14 @@
 1. Create `.env.local` file in the root directory
 
 2. Add the following lines:
-   
+
    ```markdown
    MONGO_URI=mongodb+srv://peerprep:peerprep@peerprep-g41.lvzxhrp.mongodb.net/?retryWrites=true&w=majority
    MONGO_DB=peerprep
    MONGO_COLLECTION=users
+   MONGO_QN_EASY=question_easy
+   MONGO_QN_MEDIUM=question_medium
+   MONGO_QN_HARD=question_hard
    NEXTAUTH_SECRET=0jtDHssr83fgtJKroV6DGQ6uD9BXW4+FvPbehI9zUXY=
    NEXTAUTH_URL=http://localhost:3000
    ```
--- src/pages/coderoom/[roomNum]/index.tsx
+++ src/pages/coderoom/[roomNum]/index.tsx
@@ -14,88 +14,140 @@ import useWindowDimensions from "../../../utils/useWindowDimension";
 
 import { Message } from "react-chat-ui";
 import { Session } from "next-auth";
+import axios from "axios";
 
 let socket: any;
 
-export const RoomContext = createContext<{ socket: any; roomNum: string | string[]; session: Session } | undefined>(undefined);
+export const RoomContext = createContext<
+  { socket: any; roomNum: string | string[]; session: Session } | undefined
+>(undefined);
 
 const Room = () => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
 
-    const [currentLanguage, setCurrentLanguage] = useState("javascript");
-    const [currentTheme, setCurrentTheme] = useState("vs-dark");
+  const [currentLanguage, setCurrentLanguage] = useState("javascript");
+  const [currentTheme, setCurrentTheme] = useState("vs-dark");
+  const [questionNumber, setQuestionNumber] = useState<Number>(0);
+  const [difficulty, setDifficulty] = useState("hard");
 
-    const [message, setMessage] = useState<string>("");
-    const [chats, setChats] = useState<Message[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const [chats, setChats] = useState<Message[]>([]);
 
-    const router = useRouter();
-    const { roomNum } = router.query;
+  const [questionDescription, setQuestionDescription] = useState("");
+  const [questionName, setQuestionName] = useState("");
+  const [questionDifficulty, setQuestionDifficulty] = useState("");
+  const [questionExamples, setQuestionExamples] = useState<object[]>(
+    Object(undefined)
+  );
+  const [questionTests, setQuestionTests] = useState<object[]>(
+    Object(undefined)
+  );
+  const [questionType, setQuestionType] = useState<string[]>([]);
 
-    const { data: session } = useSession();
+  const router = useRouter();
+  const { roomNum } = router.query;
 
-    useEffect(() => {
-        socketInitializer();
-    }, []);
+  const { data: session } = useSession();
 
-    const socketInitializer = async () => {
-        // Call default io
-        await fetch("/api/matching/socket");
-        socket = io();
-        socket.emit("join-room", roomNum);
+  useEffect(() => {
+    socketInitializer();
+  }, []);
 
-        socket.on("receive-collab-edit", (message: any) => {
-            setInput(message);
-        });
-
-        socket.on("message-received", (allChats: Message[]) => {
-            setChats(allChats);
-        });
-    };
-
-    function onChangeHandler(value: any, event: any) {
-        setInput(value);
-        socket.emit("collab-edit", roomNum, value);
+  useEffect(() => {
+    if (questionNumber !== 0) {
+      getQuestion();
     }
+  }, [questionNumber]);
 
-    const { height } = useWindowDimensions();
+  const getQuestion = async () => {
+    try {
+      await axios
+        .get(`../api/questions/${difficulty}/${questionNumber}`)
+        .then((res) => {
+          const question = res.data.question;
+          setQuestionName(question.name);
+          setQuestionDescription(question.description);
+          setQuestionDifficulty(question.difficulty);
+          setQuestionExamples(question.examples);
+          setQuestionTests(question.tests);
+          setQuestionType(question.type);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <RoomContext.Provider value={{ socket: socket, roomNum: roomNum!, session: session! }}>
-            <div className="dark:bg-black bg-light-100 dark:bg-opacity-20 overflow-y-hidden">
-                <StatusBar />
-                <div className="p-2">
-                    <div className="grid grid-cols-5 gap-2 pt-20">
-                        <Question />
-                        <div className="col-span-3 dark:bg-dark-100 bg-white p-4 rounded-lg container h-full">
-                            <div className="flex gap-6 items-center">
-                                <div className="flex gap-1 items-center">
-                                    <p>Change language:</p>
-                                    <LanguageSelectionListbox setCurrentLanguage={setCurrentLanguage} />
-                                </div>
-                                <div className="flex gap-1 items-center">
-                                    <p>Change theme:</p>
-                                    <ThemeSelectionListbox setCurrentTheme={setCurrentTheme} />
-                                </div>
-                            </div>
+  const socketInitializer = async () => {
+    // Call default io
+    await fetch("/api/matching/socket");
+    socket = io();
+    socket.emit("join-room", roomNum);
 
-                            <Editor
-                                height={`${height! - 235}px`}
-                                theme={currentTheme}
-                                defaultLanguage="javascript"
-                                language={currentLanguage}
-                                loading={<Loader />}
-                                value={input}
-                                onChange={onChangeHandler}
-                                className="mt-5"
-                            />
-                        </div>
+    socket.on("receive-collab-edit", (message: any) => {
+      setInput(message);
+    });
 
-                        <Chat chats={chats} socketId={socket && socket.id} />
-                    </div>
+    socket.on("message-received", (allChats: Message[]) => {
+      setChats(allChats);
+    });
+
+    socket.on("questionNumber", (qnNumber: Number) => {
+      setQuestionNumber(qnNumber);
+    });
+  };
+
+  function onChangeHandler(value: any, event: any) {
+    setInput(value);
+    socket.emit("collab-edit", roomNum, value);
+  }
+
+  const { height } = useWindowDimensions();
+
+  return (
+    <RoomContext.Provider
+      value={{ socket: socket, roomNum: roomNum!, session: session! }}
+    >
+      <div className="dark:bg-black bg-light-100 dark:bg-opacity-20 overflow-y-hidden">
+        <StatusBar />
+        <div className="p-2">
+          <div className="grid grid-cols-5 gap-2 pt-20">
+            <Question
+              questionDescription={questionDescription}
+              questionName={questionName}
+              questionDifficulty={questionDifficulty}
+            />
+            <div className="col-span-3 dark:bg-dark-100 bg-white p-4 rounded-lg container h-full">
+              <div className="flex gap-6 items-center">
+                <div className="flex gap-1 items-center">
+                  <p>Change language:</p>
+                  <LanguageSelectionListbox
+                    setCurrentLanguage={setCurrentLanguage}
+                  />
                 </div>
+                <div className="flex gap-1 items-center">
+                  <p>Change theme:</p>
+                  <ThemeSelectionListbox setCurrentTheme={setCurrentTheme} />
+                </div>
+              </div>
+
+              <Editor
+                height={`${height! - 235}px`}
+                theme={currentTheme}
+                defaultLanguage="javascript"
+                language={currentLanguage}
+                loading={<Loader />}
+                value={input}
+                onChange={onChangeHandler}
+                className="mt-5"
+              />
             </div>
-        </RoomContext.Provider>
-    );
+
+            <Chat chats={chats} socketId={socket && socket.id} />
+          </div>
+        </div>
+      </div>
+    </RoomContext.Provider>
+  );
 };
 
 export default Room;
